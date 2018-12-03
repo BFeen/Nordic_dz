01a6a78626a871b01d7bd8bcdca3a0d62866e859
Feen <bfeen@mail.ru>
2018-12-03 12:53:44 +0300

    add FAQ block

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+// Функции открытия-закрытия PopUp окна
 $('#pop-open').click(function() {
     $('.popup').toggle(true);
 });
 $('.guess__close').click(function() {
     $('.popup').toggle(false);
 })
+
+// Функция открытия ответа на вопрос FAQ
+$('.faq__item').click(function() {
+    $('.faq__answer').slideToggle(500);
+});
